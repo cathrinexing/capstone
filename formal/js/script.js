bc0ceb6de30f85977e6fa36b1be19dad1465ea5f
xing liu <31227004+cathrinexing@users.noreply.github.com>
2019-02-26 12:01:41 -0500

    w

--- formal/js/script.js
+++ formal/js/script.js
@@ -1,48 +1,81 @@
   $(document).ready(function() {
-      //jquery code in here
+    //jquery code in here
 
-      var counter = 0;
+    var counter = 0;
 
-      var container1 = false;
-      var container2 = false;
+    var container1 = false;
+    var container2 = false;
+    var container3 = false;
 
-    $("#um").click(function(){
-        // what do you want to do when you click um image
+    // reset button. click
+    // $(".factPic").html("");
+    // container1 = false;
+    //$(".factbutton").css("background","green");
+    
 
-      if(container1 == false){
+
+
+
+    //button um
+    $("#um").click(function() {
+      // what do you want to do when you click um image
+
+      if (container1 == false) {
         $("#factpic1").html('<img src="img/um.jpg" class="factPicImages" alt="um"  height="450" width="450">');
-        $("#um").css("background","yellow");
+        $("#um").css("background", "yellow");
         container1 = true;
-      }else if(container2 == false){
+      } else if (container2 == false) {
         $("#factpic2").html('<img src="img/um.jpg" class="factPicImages" alt="um"  height="450" width="450">');
-        $("#um").css("background","yellow");
+        $("#um").css("background", "yellow");
         container2 = true;
+      } else if (container3 == false) {
+        $("#factpic2").html('<img src="img/um.jpg" class="factPicImages" alt="um"  height="450" width="450">');
+        $("#um").css("background", "yellow");
+        container3 = true;
       }
     });
 
 
-    $("#parson").click(function(){
+    //button parson
+    $("#parson").click(function() {
 
-      if(container1 == false){
+      if (container1 == false) {
         $("#factpic1").html('<img src="img/parson.jpg"  alt="parsons" class="factPicImages" height="450" width="450">');
-        $("#parson").css("background","yellow");
+        $("#parson").css("background", "yellow");
         container1 = true;
-      }else if(container2 == false){
-        $("#factpic2").html('<img src="img/parson.jpg"  alt="parsons" class="factPicImages" height="450" width="450">');
-        $("#parson").css("background","yellow");
+      } else if (container2 == false) {
+        $("#factpic2").html('<img src="img/1.jpg"  alt="parsons" class="factPicImages" height="450" width="450">');
+        $("#parson").css("background", "yellow");
         container2 = true;
+      } else if (container3 == false) {
+        $("#factpic2").html('<img src="img/1.jpg"  alt="parsons" class="factPicImages" height="450" width="450">');
+        $("#parson").css("background", "yellow");
+        container3 = true;
       }
 
+    });
+
+
+    //button visual
+    $("#visual").click(function() {
+
+      if (container1 == false) {
+        $("#factpic1").html('<img src="img/parson.jpg"  alt="parsons" class="factPicImages" height="450" width="450">');
+        $("#visual").css("background", "yellow");
+        container1 = true;
+      } else if (container2 == false) {
+        $("#factpic2").html('<img src="img/1.jpg"  alt="parsons" class="factPicImages" height="450" width="450">');
+        $("#visual").css("background", "yellow");
+        container2 = true;
+      } else if (container3 == false) {
+        $("#factpic2").html('<img src="img/1.jpg"  alt="parsons" class="factPicImages" height="450" width="450">');
+        $("#visual").css("background", "yellow");
+        container3 = true;
+      }
 
     });
 
 
 
 
-
-
-
-
-
-
-  });  //end of document ready
+  }); //end of document ready
