7dc8266e5e829c713dfe9e6aba02357b4f5c1173
xing liu <31227004+cathrinexing@users.noreply.github.com>
2019-03-18 21:29:59 -0400

    w
    s

---
+++ checkbox/88.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	    var $results=$('.result').text(function(){
+	        /* swap text with catgories to see results better */
+	        return $(this).data('category')
+	    })
+	    var $checks=$(':checkbox[name^=fl]').change(function(){
+	        var $checked=$checks.filter(':checked');
+	        /* show all when nothing checked*/
+	        if(!$checked.length){
+	            $results.show();
+	            return;
+	        }
+	        /* create array of checked values */
+	        var checkedVals= $.map($checked, function(el){
+	            return el.value
+	        });
+	        /* hide all results, then filter for matches */
+	        $results.hide().filter(function(){
+	            var cats=$(this).data('category').split(' ');
+	            var checkMatches=$.grep(checkedVals, function(val){
+	                return $.inArray(val, cats) >-1;
+	            });
+	            return checkMatches.length === checkedVals.length;
+	         /* show resultas that match all checkboxes */
+	        }).show();
+	    });
+	});
