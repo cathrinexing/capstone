7cf85684157662ff48d8571f5941f75a61558341
xing liu <31227004+cathrinexing@users.noreply.github.com>
2019-03-19 14:41:27 -0400

    d

---
+++ formal copy/js/filter.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+  var $results = $('.result').text(function() {
+    /* swap text with catgories to see results better */
+    // return $(this).data('id')
+    return $(this).div
+		// return $(this).data('category')
+
+
+  })
+
+
+  var $checks = $(':checkbox[name^=fl]').change(function() {
+    var $checked = $checks.filter(':checked');
+    /* show all when nothing checked*/
+    if (!$checked.length) {
+      $results.show();
+      return;
+    }
+    /* create array of checked values */
+    var checkedVals = $.map($checked, function(el) {
+      return el.value
+    });
+    /* hide all results, then filter for matches */
+    $results.hide().filter(function() {
+      var cats = $(this).data('category').split(' ');
+      var checkMatches = $.grep(checkedVals, function(val) {
+        return $.inArray(val, cats) > -1;
+      });
+      return checkMatches.length === checkedVals.length;
+      /* show resultas that match all checkboxes */
+    }).show();
+  });
+
+
+  $('#resetfilter').click(function() {
+    // $(":checkbox").attr('checked', false);
+		$('input:checkbox').prop('checked',false);
+		$results.show();
+		return;
+  });
+
+  $('.img').hide();
+
+
+  $('#ssum').mouseenter(function(){
+$('#img1').show();
+  });
+
+  $('#ssum').mouseleave(function(){
+    $('#img1').hide();
+  });
+
+
+});
---
+++ formal copy/js/script.js
@@ -0,0 +1,113 @@
+  $(document).ready(function() {
+    //jquery code in here
+
+    var counter = 0;
+
+    var container1 = false;
+    var container2 = false;
+    // var container3 = false;
+
+    // reset button. click
+    // $(".factPic").html("");
+    // container1 = false;
+    //$(".factbutton").css("background","green");
+
+    //reset button functiobn
+    $("#reset").click(function() {
+
+       // $(".hints").html("click button to show factsheets");
+ $(".text").html("");
+ $(".picture").html("");
+
+      // $("#factpic1").html("");
+      // $("#factpic2").html("");
+
+      // $("#facttext1").html("");
+      // $("#facttext2").html("");
+
+      container1 = false;
+      container2 = false;
+      // container3 = false;
+      $(".factbutton").css("background", "lightblue");
+    });
+
+
+
+
+
+    //button um
+    $("#um").click(function() {
+      // what do you want to do when you click um image
+
+      if (container1 == false) {
+
+        $("#facttext1").html('<a href="https://interactive.miami.edu">University of Miami</a>');
+        $("#factpic1").html('<img src="img/UM.png" class="factPicImages" alt="um"  width="400">');
+        $("#um").css("background", "yellow");
+        container1 = true;
+      } else if (container2 == false) {
+        $("#facttext2").html('<h4 id="facttext1"><a href="https://interactive.miami.edu">University of Miami</h4>');
+
+
+        $("#factpic2").html('<img src="img/UM.png" class="factPicImages" alt="um"  width="400">');
+        $("#um").css("backgrxound", "yellow");
+        container2 = true;
+      }
+      //  else if (container3 == false) {
+      //   $("#factpic3").html('<img src="img/um.jpg" class="factPicImages" alt="um"  width="400">');
+      //   $("#um").css("background", "yellow");
+      //   container3 = true;
+      // }
+    });
+
+
+    //button mit
+    $("#mit").click(function() {
+
+      if (container1 == false) {
+        $("#facttext1").html('<a href="https://www.media.mit.edu/graduate-program/about-media-arts-sciences/">Massachusetts Institute of Technology</a>');
+
+        $("#factpic1").html('<img src="img/MIT.png"  alt="parsons" class="factPicImages" width="400">');
+        $("#mit").css("background", "yellow");
+        container1 = true;
+      } else if (container2 == false) {
+        $("#facttext2").html('<a href="https://www.media.mit.edu/graduate-program/about-media-arts-sciences/">Massachusetts Institute of Technology</a>');
+        $("#factpic2").html('<img src="img/MIT.png"  alt="parsons" class="factPicImages" width="400">');
+        $("#mit").css("background", "yellow");
+        container2 = true;
+      }
+      // else if (container3 == false) {
+      //   $("#factpic3").html('<img src="img/1.jpg"  alt="parsons" class="factPicImages"height="500" width="400">');
+      //   $("#mit").css("background", "yellow");
+      //   container3 = true;
+      // }
+
+    });
+
+
+    //button uwhcde
+    $("#uwhcde").click(function() {
+
+      if (container1 == false) {
+        $("#facttext1").html('<a href="https://interactive.miami.edu">University of Washington</a>');
+        $("#factpic1").html('<img src="img/uwhcde.png"  alt="parsons" class="factPicImages" width="500">');
+        $("#uwhcde").css("background", "yellow");
+        container1 = true;
+      } else if (container2 == false) {
+        $("#facttext2").html('<a href="https://interactive.miami.edu">University of Washington</a>');
+        $("#factpic2").html('<img src="img/uwhcde.png"  alt="parsons" class="factPicImages" width="500">');
+        $("#uwhcde").css("background", "yellow");
+        container2 = true;
+      }
+      // else if (container3 == false) {
+      //   $("#factpic3").html('<img src="img/1.jpg"  alt="parsons" class="factPicImages" width="500">');
+      //   $("#uwhcde").css("background", "yellow");
+      //   container3 = true;
+      // }
+
+    });
+
+
+
+
+  }); //end of document ready
