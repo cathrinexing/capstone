4f2ab5331a5482082abb87f1ef47b52deb357be6
xing liu <31227004+cathrinexing@users.noreply.github.com>
2019-03-27 11:54:15 -0400

    s

--- iii/js/script.js
+++ iii/js/script.js
@@ -14,7 +14,7 @@
 
     //reset button functiobn
     $("#reset").click(function() {
-    
+
        // $(".hints").html("click button to show factsheets");
  $(".text").html("");
  $(".picture").html("");
@@ -42,19 +42,19 @@
       if (container1 == false) {
 
         $("#facttext1").html('<a href="https://interactive.miami.edu">University of Miami</a>');
-        $("#factpic1").html('<img src="img/UM.png" class="factPicImages" alt="um"  width="400">');
+        $("#factpic1").html('<img src="img/UM.png" class="factPicImages" alt="um"  width="500">');
         $("#um").css("background", "yellow");
         container1 = true;
       } else if (container2 == false) {
         $("#facttext2").html('<h4 id="facttext1"><a href="https://interactive.miami.edu">University of Miami</h4>');
 
 
-        $("#factpic2").html('<img src="img/UM.png" class="factPicImages" alt="um"  width="400">');
+        $("#factpic2").html('<img src="img/UM.png" class="factPicImages" alt="um"  width="500">');
         $("#um").css("backgrxound", "yellow");
         container2 = true;
       }
       //  else if (container3 == false) {
-      //   $("#factpic3").html('<img src="img/um.jpg" class="factPicImages" alt="um"  width="400">');
+      //   $("#factpic3").html('<img src="img/um.jpg" class="factPicImages" alt="um"  width="500">');
       //   $("#um").css("background", "yellow");
       //   container3 = true;
       // }
@@ -67,17 +67,17 @@
       if (container1 == false) {
         $("#facttext1").html('<a href="https://www.media.mit.edu/graduate-program/about-media-arts-sciences/">Massachusetts Institute of Technology</a>');
 
-        $("#factpic1").html('<img src="img/MIT.png"  alt="parsons" class="factPicImages" width="400">');
+        $("#factpic1").html('<img src="img/MIT.png"  alt="parsons" class="factPicImages" width="500">');
         $("#parson").css("background", "yellow");
         container1 = true;
       } else if (container2 == false) {
         $("#facttext2").html('<a href="https://www.media.mit.edu/graduate-program/about-media-arts-sciences/">Massachusetts Institute of Technology</a>');
-        $("#factpic2").html('<img src="img/MIT.png"  alt="parsons" class="factPicImages" width="400">');
+        $("#factpic2").html('<img src="img/MIT.png"  alt="parsons" class="factPicImages" width="500">');
         $("#parson").css("background", "yellow");
         container2 = true;
       }
       // else if (container3 == false) {
-      //   $("#factpic3").html('<img src="img/1.jpg"  alt="parsons" class="factPicImages"height="500" width="400">');
+      //   $("#factpic3").html('<img src="img/1.jpg"  alt="parsons" class="factPicImages"height="500" width="500">');
       //   $("#parson").css("background", "yellow");
       //   container3 = true;
       // }
