1af2b666e722bb9eeddffef16770fc1afd207a8f
xing liu <31227004+cathrinexing@users.noreply.github.com>
2019-05-13 16:47:38 -0400

    hh

---
+++ learningtest/test.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+  $("#bee").click(function(){
+
+    $(".dots").show();
+    $(".dots").css("background", "lightblue");
+    $(".circle").html("xxx");
+
+
+  });//end of click bee
+
+
+
+});//end of document function
+
+
+
+for (var i = 0; i < array.length; i++) {
+  array[i]
+}
+
+if (true) {
+
+}
+else {
+  
+}
